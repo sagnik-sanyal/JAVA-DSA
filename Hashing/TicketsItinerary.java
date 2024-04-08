702845cda4acc641b86ec653df59f7031c41a288
SAGNIK SANYAL <sagniksanyal1502@gmail.com>
2024-04-08 12:17:36 +0530

    add commit

--- Hashing/TicketsItinerary.java
+++ Hashing/TicketsItinerary.java
@@ -3,11 +3,11 @@ package Hashing;
 public class TicketsItinerary {
 
     public static void main(String[] args) {
-        String[][] tickets = {{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
+//        String[][] tickets = {{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
 //        System.out.println(findItinerary(tickets));
     }
 
-//    private static boolean findItinerary(String[][] tickets) {
+//    private static void findItinerary(String[][] tickets) {
 //
 //    }
 
